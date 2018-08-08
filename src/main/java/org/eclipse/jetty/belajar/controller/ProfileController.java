15148e6ecd092e0295766a32a2c0453b14a77328
flashdin <flashdin007@gmail.com>
2018-08-08 14:21:22 +0700

    edit ProfileController

--- src/main/java/org/eclipse/jetty/belajar/controller/ProfileController.java
+++ src/main/java/org/eclipse/jetty/belajar/controller/ProfileController.java
@@ -1,12 +1,17 @@
 package org.eclipse.jetty.belajar.controller;
 
 
+import org.eclipse.jetty.belajar.entity.Profile;
 import org.eclipse.jetty.belajar.service.ProfileService;
 import org.eclipse.jetty.belajar.service.impl.ProfileServiceImpl;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class ProfileController extends HttpServlet {
@@ -16,27 +21,47 @@ public class ProfileController extends HttpServlet {
     private ProfileService profileService = new ProfileServiceImpl();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String act = req.getServletPath();
+        RequestDispatcher dispatcher;
         switch (act) {
             case "/profile":
-                /*profileService.findAll(req, resp);*/
+                List<Profile> profiles = profileService.findAll();
+                req.setAttribute("dataSets", profiles);
+                dispatcher = req.getRequestDispatcher("/profile/list.jsp");
+                dispatcher.forward(req, resp);
                 break;
             case "/profile/create":
-                /*profileService.viewCreate(req, resp);*/
+                dispatcher = req.getRequestDispatcher("/profile/create.jsp");
+                dispatcher.forward(req, resp);
                 break;
             case "/profile/update":
-                /*profileService.viewUpdate(req, resp);*/
+                Profile profile = new Profile();
+                profile.setId(Integer.valueOf(req.getParameter("id")));
+                Profile data = profileService.findById(profile);
+                req.setAttribute("dataSets", data);
+                dispatcher = req.getRequestDispatcher("/profile/update.jsp");
+                dispatcher.forward(req, resp);
                 break;
         }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String act = req.getServletPath();
         switch (act) {
             case "/profile/create":
-                /*profileService.save(req, resp);*/
+                Profile profile = new Profile();
+                profile.setNamaLengkap(req.getParameter("txtNama"));
+                profile.setAlamat(req.getParameter("txtAlamat"));
+                profile.setUmur(Integer.valueOf(req.getParameter("txtUmur")));
+                profile.setJk(req.getParameter("cmbJk"));
+                Profile data = profileService.save(profile);
+                if (data == null) {
+                    resp.sendRedirect("/profile/create?success=0");
+                } else {
+                    resp.sendRedirect("/profile/create?success=1");
+                }
                 break;
             case "/profile/update":
                 this.doPut(req, resp);
@@ -48,61 +73,14 @@ public class ProfileController extends HttpServlet {
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
-        /*profileService.update(req, resp);*/
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-        /*profileService.delete(req, resp);*/
-    }
-}
-
-
-    // TODO Pindah disini, jadi service cuma mengolah data.
-    /*public void findAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Profile> profiles = profileDAO.findAll();
-        req.setAttribute("dataSets", profiles);
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/profile/list.jsp");
-        dispatcher.forward(req, resp);
-    }
-
-    public void viewCreate(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/profile/create.jsp");
-        dispatcher.forward(req, resp);
-    }
-
-    public void viewUpdate(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Profile profile = new Profile();
-        profile.setId(Integer.valueOf(req.getParameter("id")));
-        Profile data = profileDAO.findById(profile);
-        req.setAttribute("dataSets", data);
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/profile/update.jsp");
-        dispatcher.forward(req, resp);
-    }
-
-    public void save(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        Profile profile = new Profile();
-        profile.setNamaLengkap(req.getParameter("txtNama"));
-        profile.setAlamat(req.getParameter("txtAlamat"));
-        profile.setUmur(Integer.valueOf(req.getParameter("txtUmur")));
-        profile.setJk(req.getParameter("cmbJk"));
-        Profile data = profileDAO.save(profile);
-        if (data == null) {
-            resp.sendRedirect("/profile/create?success=0");
-        } else {
-            resp.sendRedirect("/profile/create?success=1");
-        }
-    }
-
-    public void update(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Profile profile = new Profile();
         profile.setNamaLengkap(req.getParameter("txtNama"));
         profile.setAlamat(req.getParameter("txtAlamat"));
         profile.setUmur(Integer.valueOf(req.getParameter("txtUmur")));
         profile.setJk(req.getParameter("cmbJk"));
         profile.setId(Integer.valueOf(req.getParameter("id")));
-        Profile data = profileDAO.update(profile);
+        Profile data = profileService.update(profile);
         if (data == null) {
             resp.sendRedirect("/profile?usuccess=0");
         } else {
@@ -110,13 +88,16 @@ public class ProfileController extends HttpServlet {
         }
     }
 
-    public void delete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Profile profile = new Profile();
         profile.setId(Integer.valueOf(req.getParameter("id")));
-        int data = profileDAO.delete(profile);
+        int data = profileService.delete(profile);
         if (data == 0) {
             resp.sendRedirect("/profile?dsuccess=0");
         } else {
             resp.sendRedirect("/profile?dsuccess=1");
         }
-    }*/
+    }
+    
+}
